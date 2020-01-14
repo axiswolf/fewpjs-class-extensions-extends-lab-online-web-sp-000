f95acda15ce204a1926c9c45e674f000babb3531
Karen V <karenvo@usc.edu>
2020-01-14 06:29:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,5 +24,17 @@ class Square extends Polygon {
   get isValid() {
     // checks if there is 4 sides
     // checks if each side is equal
+    function checkSides() {
+     const perimeter = this.perimeter
+     return this.sides.some((side) => (perimeter - side) <= side)
+   }
+
+   if (this.getCount !== 3) {
+     return false
+   } else if (checkSides.call(this)) {
+     return false
+   } else {
+     return true
+   }
   }
 }
